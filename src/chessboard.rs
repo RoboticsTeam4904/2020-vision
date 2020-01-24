151ebb9da666adaef5da6072a0d8e01df8f4eeda
Yash Narayan <yash.nara04@gmail.com>
2020-01-23 18:01:53 -0800

    added comments.

--- src/chessboard.rs
+++ src/chessboard.rs
@@ -6,11 +6,15 @@ use opencv::{
 
 pub fn find_chessboard (
     image: &dyn ToInputArray, 
+    // image goes to find outside corners
+    // find oustide corners gives output of corners array
+    // using image and output corners pass it into find_chesboard_corners
+    // return the corners from the find_chessboard_corners
     pattern_size: Size, 
+    let corners =
     corners: &mut dyn ToOutputArray, 
     flags: i32
 ) -> Result<bool>
-
 
 
 
