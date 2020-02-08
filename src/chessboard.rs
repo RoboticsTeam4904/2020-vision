f1d9f6b2e0ec8131dc3e0e4eb0c1ef953a95f51a
IanLum <ianlum314@gmail.com>
2020-02-07 18:55:31 -0800

    chessboard finds rotations, roll and yaw are swapped, changed corners from 7*7 to 9*6

--- src/chessboard.rs
+++ src/chessboard.rs
@@ -2,39 +2,55 @@ use opencv::{
     calib3d::{
         draw_chessboard_corners, find_chessboard_corners, rodrigues, solve_pnp, CALIB_CB_FAST_CHECK,
     },
-    core::{Mat, Size, Image2D},
-    imgcodecs::imwrite,
+    core::{Mat, Size},
+    imgcodecs::{imwrite, IMWRITE_JPEG_QUALITY},
     prelude::*,
-    types::{VectorOfPoint2f, VectorOfPoint3f},
+    types::{VectorOfPoint2f, VectorOfPoint3f, VectorOfint},
 };
 
-use standard_vision::{
+use stdvis_core::{
     traits::ImageData,
     types::{CameraConfig, Image},
 };
 
-use opencv_camera::image::AsMat;
+use stdvis_opencv::convert::{AsArrayView, AsMatView};
 
-pub fn find_chessboard<I: ImageData>(
-    image: &Image<I>,
-    obj_points: &VectorOfPoint3f,
-    config: &CameraConfig,
-) {
-    let image_mat: Mat = image.as_mat();
-    let board_size = Size::new(7, 7);
+use ndarray::prelude::*;
+
+pub fn find_chessboard<I: ImageData>(image: &Image<I>, obj_points: &VectorOfPoint3f, config: &CameraConfig, board_size: Size) {
+    let mut flag = VectorOfint::new();
+    flag.push(IMWRITE_JPEG_QUALITY);
+    flag.push(95);
+
+    let mut image_mat: Mat = image.as_mat_view().clone().unwrap();
     let mut corners = VectorOfPoint2f::new();
 
-    find_chessboard_corners(&image_mat, board_size, &mut corners, CALIB_CB_FAST_CHECK);
+    imwrite("b.jpg", &image_mat, &flag);
+
+    let corners_found =
+        find_chessboard_corners(&image_mat, board_size, &mut corners, CALIB_CB_FAST_CHECK).unwrap();
+
+    if !corners_found {
+        return;
+    }
+    
+    draw_chessboard_corners(&mut image_mat, board_size, &corners, corners_found);
+    imwrite("chess_corners.jpg", &image_mat, &flag);
 
     let mut rvec = Mat::default().unwrap();
-    let mut tvec = Mat::default().unwrap();
+    let mut tvec_mat = Mat::default().unwrap();
     let dist_coeffs = Mat::default().unwrap();
-    let camera_mat = Mat::from_slice_2d(&[
-        [config.focal_length, 0.0, ((config.resolution.0) / 2) as f64],
-        [0.0, config.focal_length, ((config.resolution.1) / 2) as f64],
-        [0.0, 0.0, 1.0],
-    ])
-    .unwrap();
+
+    let fx = config.resolution.0 as f64 / config.sensor_width;
+    let fy = config.resolution.1 as f64 / config.sensor_height;
+    let cx = config.resolution.0 as f64 / 2.;
+    let cy = config.resolution.1 as f64 / 2.;
+    let camera_mat =
+        Mat::from_slice_2d(&[
+            [fx, 0.0, cx],
+            [0.0, fy, cy],
+            [0.0, 0.0, 1.0]
+            ]).unwrap();
 
     solve_pnp(
         &obj_points,
@@ -42,13 +58,28 @@ pub fn find_chessboard<I: ImageData>(
         &camera_mat,
         &dist_coeffs,
         &mut rvec,
-        &mut tvec,
+        &mut tvec_mat,
         false,
-        opencv::calib3d::SOLVEPNP_IPPE,
+        opencv::calib3d::SOLVEPNP_ITERATIVE,
     );
 
-    let mut rmat = Mat::default().unwrap();
-    let mut jacobian = Mat::default().unwrap();
+    let mut rmat_mat = Mat::default().unwrap();
+    let mut jacobian_mat = Mat::default().unwrap();
 
-    rodrigues(&rvec, &mut rmat, &mut jacobian);
+    rodrigues(&rvec, &mut rmat_mat, &mut jacobian_mat);
+
+    let tvec = tvec_mat.as_array_view::<f64>().into_shape((3, 1)).unwrap();
+    let rmat = rmat_mat.as_array_view::<f64>().into_shape((3, 3,)).unwrap();
+    let rmat_trans = rmat.t();
+    
+    let camera_pose = rmat_trans.dot(&tvec);
+
+    let yaw = -rmat_trans[[1,0]].atan2(rmat_trans[[0,0]]);
+
+    println!("---");
+    println!("yaw: {}", yaw.to_degrees());
+    println!("pitch: {}", rmat_trans[[2,0]].asin().to_degrees());
+    println!("roll: {}", (-rmat_trans[[2,1]].atan2(rmat_trans[[2,2]])).to_degrees());
+
+    // println!("distance: {}", camera_pose[[2, 0]]);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,54 @@
 // mod analysis;
 mod chessboard;
 
-use control_panel::ControlPanelTracker;
-use standard_vision::types::{CameraConfig, Pose};
 use chessboard::find_chessboard;
-
+use opencv::{
+    core::{Point3f, Size},
+    prelude::*,
+    types::VectorOfPoint3f,
+};
+use stdvis_core::{
+    traits::Camera,
+    types::{CameraConfig, Pose},
+};
+use stdvis_opencv::camera::OpenCVCamera;
 
 fn main() {
-    let b = ControlPanelTracker {};
-    let config = CameraConfig { // config for c920
-        id: 0,
-        resolution: (1080, 720),
-        pose: Pose {
+    let mut camera = OpenCVCamera::new(
+        0,
+        Pose {
             x: 0,
             y: 0,
             z: 0,
             angle: 0.0,
         },
-        fov: (70.42, 43.3),
-        focal_length: 3.67,
-        sensor_height: 3.6,
-    };
+        (70.42, 43.3),
+        3.67,
+        3.6,
+        4.8,
+    )
+    .unwrap();
 
-    let mut camera = OpenCVCamera::new_from_index(0, config.pose, config.fov, config.focal_length, config.sensor_height).unwrap();
-    let image = camera.grab_frame().unwrap();
-    let image_mat: Mat = image.as_mat();
+    const CHESS_WIDTH: f32 = 123.;
+    const CHESS_HEIGHT: f32 = 77.;
 
+    let mut obj_points = VectorOfPoint3f::new();
+    let config = camera.config();
+    let board_size = Size::new(9, 6);
+    let distance = config.focal_length as f32 * CHESS_HEIGHT / config.sensor_height as f32;
+
+    for i in 0..board_size.height {
+        for j in 0..board_size.width {
+            obj_points.push(Point3f::new(
+                CHESS_WIDTH / 2. - CHESS_WIDTH * j as f32 / (board_size.width - 1) as f32,
+                CHESS_HEIGHT / 2. - CHESS_HEIGHT * i as f32 / (board_size.height - 1) as f32,
+                distance.clone(),
+            ));
+        }
+    }
+
+    loop {
+        let image = camera.grab_frame().unwrap();
+        find_chessboard(&image, &obj_points, &config, board_size);
+    }
 }
