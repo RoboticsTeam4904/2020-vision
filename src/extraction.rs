3e3eec6e192e4923141a1577d77607a0441993f2
aemino <aeminodev@gmail.com>
2020-02-10 16:23:25 -0800

    Clean up extraction code

--- src/extraction.rs
+++ src/extraction.rs
@@ -1,26 +1,28 @@
 use std::ops::Range;
 
-use opencv::{imgproc, prelude::*};
+use opencv::{
+    imgproc,
+    prelude::*,
+    types::{VectorOfPoint, VectorOfVectorOfPoint},
+};
 
-use standard_vision::{
+use stdvis_core::{
     traits::{ContourExtractor, ImageData},
     types::{Contour, Image},
 };
 
-use opencv_camera::image::AsMat;
+use stdvis_opencv::convert::AsMatView;
 
 const RFTAPE_HSV_RANGE: Range<[u8; 3]> = [50, 103, 150]..[94, 255, 255];
 
 pub(crate) struct HighPortContourExtractor {}
 
-impl ContourExtractor for HighPortContourExtractor {
-    fn extract_from<I: ImageData>(&self, image: &Image<I>) -> Vec<Contour> {
-        let image_mat = image.as_mat();
+impl HighPortContourExtractor {
+    fn threshold_image(&self, image: &Mat) -> Mat {
         let mut hsv_image = Mat::default().unwrap();
-        let mut thresholded_image = Mat::default().unwrap();
-        let mut contours = Mat::default().unwrap();
+        imgproc::cvt_color(&image, &mut hsv_image, imgproc::COLOR_BGR2HSV, 0).unwrap();
 
-        imgproc::cvt_color(&image_mat, &mut hsv_image, imgproc::COLOR_BGR2HSV, 0).unwrap();
+        let mut thresholded_image = Mat::default().unwrap();
         opencv::core::in_range(
             &hsv_image,
             &Mat::from_slice(&RFTAPE_HSV_RANGE.start).unwrap(),
@@ -29,8 +31,13 @@ impl ContourExtractor for HighPortContourExtractor {
         )
         .unwrap();
 
+        thresholded_image
+    }
+
+    fn find_contours(&self, image: &Mat) -> VectorOfVectorOfPoint {
+        let mut contours = VectorOfVectorOfPoint::new();
         imgproc::find_contours(
-            &mut thresholded_image,
+            &image,
             &mut contours,
             imgproc::RETR_EXTERNAL,
             imgproc::CHAIN_APPROX_SIMPLE,
@@ -38,7 +45,26 @@ impl ContourExtractor for HighPortContourExtractor {
         )
         .unwrap();
 
-        todo!();
+        contours
+    }
+
+    fn filter_contour(&self, contours: &VectorOfPoint) -> bool {
+        todo!()
+    }
+}
+
+impl ContourExtractor for HighPortContourExtractor {
+    fn extract_from<I: ImageData>(&self, image: &Image<I>) -> Vec<Contour> {
+        let image_mat = image.as_mat_view();
+
+        let thresholded_image = self.threshold_image(&image_mat);
+        let contours = self.find_contours(&thresholded_image);
+
+        let filtered_contours = contours
+            .iter()
+            .filter(|contour| self.filter_contour(contour));
+
+        vec![]
     }
 }
 
@@ -46,36 +72,59 @@ impl ContourExtractor for HighPortContourExtractor {
 mod tests {
     use super::*;
 
+    use std::{rc::Rc, time};
+
+    use stdvis_core::types::CameraConfig;
+    use stdvis_opencv::camera::MatImageData;
+
     #[test]
     fn test_contour_matching() {
         use opencv::imgcodecs;
+        use std::fs;
 
-        let image = imgcodecs::imread(
-            "tests/images/BlueGoal-060in-Center.jpg",
-            imgcodecs::IMREAD_COLOR,
-        )
-        .unwrap();
+        let images = fs::read_dir("tests/images")
+            .unwrap()
+            .filter(|e| e.as_ref().unwrap().file_type().unwrap().is_file());
 
-        let mut hsv_image = Mat::default().unwrap();
-        let mut thresholded_image = Mat::default().unwrap();
-        let mut contours = Mat::default().unwrap();
+        for image in images {
+            let image_path = image.unwrap().path();
+            let image_name = image_path.file_name().unwrap().to_str().unwrap();
 
-        imgproc::cvt_color(&image, &mut hsv_image, imgproc::COLOR_BGR2HSV, 0).unwrap();
-        opencv::core::in_range(
-            &hsv_image,
-            &Mat::from_slice(&RFTAPE_HSV_RANGE.start).unwrap(),
-            &Mat::from_slice(&RFTAPE_HSV_RANGE.end).unwrap(),
-            &mut thresholded_image,
-        )
-        .unwrap();
+            let image_mat =
+                imgcodecs::imread(image_path.to_str().unwrap(), imgcodecs::IMREAD_COLOR).unwrap();
 
-        imgproc::find_contours(
-            &mut thresholded_image,
-            &mut contours,
-            imgproc::RETR_EXTERNAL,
-            imgproc::CHAIN_APPROX_SIMPLE,
-            opencv::core::Point::new(0, 0),
-        )
-        .unwrap();
+            let image = Image::new(
+                time::SystemTime::now(),
+                Rc::new(CameraConfig::default()),
+                MatImageData::new(image_mat),
+            );
+
+            let extractor = HighPortContourExtractor {};
+            let thresholded_image = extractor.threshold_image(&image.as_mat_view());
+            let contours = extractor.find_contours(&thresholded_image);
+
+            let mut out_image = image.as_mat_view().clone().unwrap();
+            imgproc::draw_contours(
+                &mut out_image,
+                &contours,
+                -1,
+                opencv::core::Scalar::new(255., 255., 255., 255.),
+                3,
+                imgproc::LINE_AA,
+                &Mat::default().unwrap(),
+                std::i32::MAX,
+                opencv::core::Point::new(0, 0),
+            )
+            .unwrap();
+
+            println!("tests/contours/{}", image_name);
+
+            imgcodecs::imwrite(
+                format!("tests/contours/{}", image_name).as_str(),
+                &out_image,
+                &opencv::types::VectorOfint::with_capacity(0),
+            )
+            .unwrap();
+        }
     }
 }
