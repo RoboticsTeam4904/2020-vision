9ddc92f7d7a90776640a958c69c2905d3e40fa5b
aemino <aeminodev@gmail.com>
2020-02-14 08:38:24 -0800

    Add contour filtering and vertex selecting/sorting to extractor

--- src/extraction.rs
+++ src/extraction.rs
@@ -1,24 +1,25 @@
 use std::ops::Range;
 
 use opencv::{
+    core::Point,
     imgproc,
     prelude::*,
-    types::{VectorOfPoint, VectorOfVectorOfPoint},
+    types::{VectorOfPoint, VectorOfVectorOfPoint, VectorOfVec4i},
 };
 
 use stdvis_core::{
     traits::{ContourExtractor, ImageData},
-    types::{Contour, Image},
+    types::{Contour, ContourGroup, Image},
 };
 
 use stdvis_opencv::convert::AsMatView;
 
-const RFTAPE_HSV_RANGE: Range<[u8; 3]> = [50, 103, 150]..[94, 255, 255];
+pub(crate) struct RFTapeContourExtractor {}
 
-pub(crate) struct HighPortContourExtractor {}
-
-impl HighPortContourExtractor {
+impl RFTapeContourExtractor {
     fn threshold_image(&self, image: &Mat) -> Mat {
+        const RFTAPE_HSV_RANGE: Range<[u8; 3]> = [50, 103, 125]..[94, 255, 255];
+
         let mut hsv_image = Mat::default().unwrap();
         imgproc::cvt_color(&image, &mut hsv_image, imgproc::COLOR_BGR2HSV, 0).unwrap();
 
@@ -34,37 +35,211 @@ impl HighPortContourExtractor {
         thresholded_image
     }
 
-    fn find_contours(&self, image: &Mat) -> VectorOfVectorOfPoint {
+    fn find_contours(&self, image: &Mat) -> Vec<VectorOfVectorOfPoint> {
         let mut contours = VectorOfVectorOfPoint::new();
-        imgproc::find_contours(
+        let mut hierarchy = VectorOfVec4i::new();
+        imgproc::find_contours_with_hierarchy(
             &image,
             &mut contours,
-            imgproc::RETR_EXTERNAL,
+            &mut hierarchy,
+            imgproc::RETR_TREE,
             imgproc::CHAIN_APPROX_SIMPLE,
             opencv::core::Point::new(0, 0),
         )
         .unwrap();
 
-        contours
+        let mut groups: Vec<Vec<i32>> = Vec::new();
+
+        for (contour_index, indices) in hierarchy.iter().enumerate() {
+            let parent_index = indices.get(3).unwrap();
+            let group_indices = match groups
+                .iter_mut()
+                .find(|indices| indices.contains(&parent_index))
+            {
+                Some(indices) => indices,
+                None => {
+                    groups.push(Vec::new());
+                    groups.last_mut().unwrap()
+                }
+            };
+
+            group_indices.push(contour_index as i32);
+        }
+
+        groups
+            .iter()
+            .map(|indices| {
+                VectorOfVectorOfPoint::from_iter(
+                    indices
+                        .iter()
+                        .map(|index| contours.get(index.clone() as usize).unwrap()),
+                )
+            })
+            .collect()
     }
 
-    fn filter_contour(&self, contours: &VectorOfPoint) -> bool {
-        todo!()
+    fn filter_high_port(&self, contours: &VectorOfVectorOfPoint) -> bool {
+        const SOLIDITY_RANGE: Range<f64> = 0.15..0.25;
+
+        if contours.len() != 1 {
+            return false;
+        }
+
+        let external_contour = contours.get(0).unwrap();
+
+        let mut hull_contour = VectorOfPoint::new();
+        imgproc::convex_hull(&external_contour, &mut hull_contour, true, false).unwrap();
+
+        let contour_area = imgproc::contour_area(&external_contour, false).unwrap();
+        let hull_area = imgproc::contour_area(&hull_contour, false).unwrap();
+
+        let solidity = contour_area / hull_area;
+
+        if !SOLIDITY_RANGE.contains(&solidity) {
+            return false;
+        }
+
+        true
+    }
+
+    fn filter_loading_port(&self, contours: &VectorOfVectorOfPoint) -> bool {
+        const SOLIDITY_RANGE: Range<f64> = 0.25..0.50;
+        const ASPECT_RATIO_RANGE: Range<f32> = 1.40..1.60;
+
+        if contours.len() != 2 {
+            return false;
+        }
+
+        let external_contour = contours.get(0).unwrap();
+        let internal_contour = contours.get(1).unwrap();
+
+        let external_area = imgproc::contour_area(&external_contour, false).unwrap();
+        let internal_area = imgproc::contour_area(&internal_contour, false).unwrap();
+
+        let solidity = internal_area / external_area;
+
+        if !SOLIDITY_RANGE.contains(&solidity) {
+            return false;
+        }
+
+        let rect = imgproc::min_area_rect(&external_contour).unwrap();
+        let rect_size = rect.size().unwrap();
+        let aspect_ratio = rect_size.width / rect_size.height;
+
+        if !ASPECT_RATIO_RANGE.contains(&aspect_ratio) {
+            return false;
+        }
+
+        true
+    }
+
+    fn find_vertices(&self, target_num_vertices: usize, contour: &VectorOfPoint) -> VectorOfPoint {
+        let mut poly_contour = VectorOfPoint::new();
+        let mut epsilon = 0.0;
+
+        loop {
+            epsilon += 0.01;
+            imgproc::approx_poly_dp(&contour, &mut poly_contour, epsilon, true).unwrap();
+
+            if poly_contour.len() <= target_num_vertices {
+                break poly_contour;
+            }
+        }
+    }
+
+    fn order_vertices(&self, contour: &VectorOfPoint) -> Contour {
+        let num_points = contour.len();
+        let mut centroid = contour
+            .iter()
+            .fold((0, 0), |acc, p| (acc.0 + p.x, acc.1 + p.y));
+
+        centroid = (
+            centroid.0 / num_points as i32,
+            centroid.1 / num_points as i32,
+        );
+
+        let vertex_theta = |point: &Point| -> f32 {
+            let theta = (-(point.y - centroid.1) as f32).atan2((point.x - centroid.0) as f32);
+
+            if theta.is_sign_positive() {
+                theta
+            } else {
+                2. * std::f32::consts::PI + theta
+            }
+        };
+
+        let (first_idx, _) = contour
+            .iter()
+            .enumerate()
+            .min_by(|(_, a), (_, b)| vertex_theta(a).partial_cmp(&vertex_theta(b)).unwrap())
+            .unwrap();
+
+        let signed_area = (1..=num_points).fold(0, |acc, idx| {
+            let p0 = contour.get(idx - 1).unwrap();
+            let p1 = contour.get(idx % num_points).unwrap();
+
+            acc + (p1.x - p0.x) * (p1.y + p0.y)
+        });
+
+        let is_clockwise = signed_area.is_negative();
+
+        let mut points = contour
+            .iter()
+            .map(|point| (point.x as u32, point.y as u32))
+            .collect::<Vec<_>>();
+        
+        if is_clockwise {
+            points.rotate_right(num_points - first_idx - 1);
+            points.reverse();
+        } else {
+            points.rotate_left(first_idx);
+        }
+
+        Contour { points }
+    }
+
+    fn extract_matching_contours<'a, F>(
+        &'a self,
+        contour_groups: &'a Vec<VectorOfVectorOfPoint>,
+        id: u8,
+        target_num_vertices: usize,
+        filter_predicate: F,
+    ) -> impl Iterator<Item = ContourGroup> + 'a
+    where
+        F: FnMut(&&VectorOfVectorOfPoint) -> bool + 'a,
+    {
+        contour_groups
+            .iter()
+            .filter(filter_predicate)
+            .map(move |contours| ContourGroup {
+                id,
+                contours: contours
+                    .iter()
+                    .map(|contour| {
+                        self.order_vertices(&self.find_vertices(target_num_vertices, &contour))
+                    })
+                    .collect(),
+            })
     }
 }
 
-impl ContourExtractor for HighPortContourExtractor {
-    fn extract_from<I: ImageData>(&self, image: &Image<I>) -> Vec<Contour> {
+impl ContourExtractor for RFTapeContourExtractor {
+    fn extract_from<I: ImageData>(&self, image: &Image<I>) -> Vec<ContourGroup> {
         let image_mat = image.as_mat_view();
 
         let thresholded_image = self.threshold_image(&image_mat);
-        let contours = self.find_contours(&thresholded_image);
+        let contour_groups = self.find_contours(&thresholded_image);
 
-        let filtered_contours = contours
-            .iter()
-            .filter(|contour| self.filter_contour(contour));
+        let high_port_contours = self.extract_matching_contours(&contour_groups, 0, 8, |contours| {
+            self.filter_high_port(contours)
+        });
 
-        vec![]
+        let loading_port_contours =
+            self.extract_matching_contours(&contour_groups, 1, 4, |contours| {
+                self.filter_loading_port(contours)
+            });
+
+        high_port_contours.chain(loading_port_contours).collect()
     }
 }
 
