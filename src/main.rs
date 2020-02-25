a5a7293ab384386e7db5a4e864e1f66ae2a175d4
IanLum <ianlum314@gmail.com>
2020-02-24 16:14:44 -0800

    add correct values to CameraConfig

--- src/main.rs
+++ src/main.rs
@@ -9,48 +9,67 @@ use opencv::{
     prelude::*,
     types::VectorOfPoint3f,
 };
+use ndarray::prelude::*;
 use stdvis_core::{
     traits::Camera,
-    types::Pose,
+    types::{Pose, CameraConfig},
 };
 use stdvis_opencv::camera::OpenCVCamera;
 
 fn main() {
     let mut camera = OpenCVCamera::new(
-        0,
-        Pose {
-            x: 0,
-            y: 0,
-            z: 0,
-            angle: 0.0,
-        },
-        (70.42, 43.3),
-        3.67,
-        3.6,
-        4.8,
+        CameraConfig {
+            id: 0,
+            resolution: (1920, 1080),
+            pose: Pose {
+                angle: 0.0,
+                dist: 0.0,
+                height: 0.0,
+                yaw: 0.0,
+                pitch: 0.0,
+                roll: 0.0,  
+            },
+            fov: (70.42, 43.3),
+            distortion_coeffs: array![
+                0.16882939064608002,
+                -0.9243844884626233,
+                0.006368686931626428,
+                0.0055936556910287875,
+                2.0772772661977683,
+            ],
+            intrinsic_matrix: array![
+                [1449.6970632013845, 0.0, 919.7416002537354],
+                [0.0, 1456.6938719499683, 549.6275213145884],
+                [0.0, 0.0, 1.0]
+            ]
+        }
     )
     .unwrap();
 
-    const CHESS_WIDTH: f32 = 123. / 1000.;
-    const CHESS_HEIGHT: f32 = 77. / 1000.;
+    const SQUARE_SIZE: f32 = 18.9 / 1000.;
 
     let mut obj_points = VectorOfPoint3f::new();
-    let config = camera.config();
-    let board_size = Size::new(9, 6);
-    let distance = config.focal_length as f32 * CHESS_HEIGHT / config.sensor_height as f32;
+    let board_size = Size::new(8, 5);
+
+    let chess_width = board_size.width as f32 * SQUARE_SIZE;
+    let chess_height = board_size.height as f32 * SQUARE_SIZE;
 
     for i in 0..board_size.height {
         for j in 0..board_size.width {
             obj_points.push(Point3f::new(
-                CHESS_WIDTH / (board_size.width - 1) as f32 * j as f32 - CHESS_WIDTH * 0.5,
-                CHESS_HEIGHT / (board_size.height - 1) as f32 * i as f32 - CHESS_HEIGHT * 0.5,
+                chess_width / (board_size.width - 1) as f32 * j as f32 - chess_width * 0.5,
+                chess_height / (board_size.height - 1) as f32 * i as f32 - chess_height * 0.5,
                 0.,
             ));
         }
     }
 
+    opencv::highgui::named_window("chess", opencv::highgui::WINDOW_NORMAL).unwrap();
+
     loop {
         let image = camera.grab_frame().unwrap();
-        find_chessboard(&image, &obj_points, &config, board_size);
+        find_chessboard(&image, &obj_points, board_size);
+
+        opencv::highgui::wait_key(30).unwrap();
     }
 }
