6f4a262a241f3624ab3b9146d1c029a6fd6b6704
aemino <aeminodev@gmail.com>
2020-03-02 13:04:12 -0800

    Use generic solvepnp for analysis

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,8 +1,8 @@
 use opencv::{
-    calib3d::{rodrigues, solve_pnp},
+    calib3d::{rodrigues, solve_pnp_generic, SolvePnPMethod},
     core::{Mat, Point2f, Point3f},
     prelude::*,
-    types::{VectorOfPoint2f, VectorOfPoint3f},
+    types::{VectorOfPoint2f, VectorOfPoint3f, VectorOfMat},
 };
 
 use stdvis_core::{
@@ -21,6 +21,7 @@ const HIGH_PORT_OBJECT_POINTS: &[(f32, f32)] = &[
     (-0.5, 0.2165),
     (-0.25, -0.2165),
     (0.25, -0.2165),
+    (0.3333, 0.0721), // symmetry-breaking point
 ];
 const LOADING_PORT_OBJECT_POINTS: &[(f32, f32)] = &[
     (0.0889, 0.1397),
@@ -31,7 +32,14 @@ const LOADING_PORT_OBJECT_POINTS: &[(f32, f32)] = &[
     (-0.0381, 0.0889),
     (-0.0381, -0.0889),
     (0.0381, -0.0889),
+    (-0.0254, -0.2286), // symmetry-breaking point
 ];
+
+pub struct PnPParams<'src> {
+    pub img_points: VectorOfPoint2f,
+    pub obj_points: &'src VectorOfPoint3f,
+    pub config: &'src CameraConfig,
+}
 
 pub struct PnPResult {
     pub intrinsic_matrix: Mat,
@@ -65,14 +73,39 @@ impl WallTapeContourAnalyzer {
         }
     }
 
+    pub fn make_pnp_params<'src>(&'src self, contour_group: &'src ContourGroup) -> PnPParams<'src> {
+        let obj_points = match contour_group.id {
+            0 => &self.high_port_object_points,
+            1 => &self.loading_port_object_points,
+            _ => panic!("Unknown contour group type"),
+        };
+
+        let img_points = VectorOfPoint2f::from_iter(
+            contour_group
+                .contours
+                .iter()
+                .map(|contour| contour.points.iter())
+                .flatten()
+                .map(|point| Point2f::new(point.0 as f32, point.1 as f32)),
+        );
+
+        let config = contour_group.camera;
+
+        PnPParams {
+            img_points,
+            obj_points,
+            config,
+        }
+    }
+
     pub fn solve_pnp(
         &self,
-        img_points: &VectorOfPoint2f,
-        obj_points: &VectorOfPoint3f,
-        config: &CameraConfig,
-    ) -> PnPResult {
-        let mut rvec_mat = Mat::default().unwrap();
-        let mut tvec_mat = Mat::default().unwrap();
+        params: PnPParams,
+    ) -> Vec<PnPResult> {
+        let PnPParams { img_points, obj_points, config } = params;
+
+        let mut rvec_mats = VectorOfMat::new();
+        let mut tvec_mats = VectorOfMat::new();
 
         // TODO: Would like to use as_mat_view(), but it currently does not work for
         // 2D matrices.
@@ -88,40 +121,53 @@ impl WallTapeContourAnalyzer {
 
         let distortion_coeffs = Mat::from_slice(d.as_slice().unwrap()).unwrap();
 
-        solve_pnp(
+        solve_pnp_generic(
             &obj_points,
             &img_points,
             &intrinsic_matrix,
             &distortion_coeffs,
-            &mut rvec_mat,
-            &mut tvec_mat,
+            &mut rvec_mats,
+            &mut tvec_mats,
             false,
-            opencv::calib3d::SOLVEPNP_IPPE,
+            SolvePnPMethod::SOLVEPNP_IPPE,
+            &opencv::core::no_array().unwrap(),
+            &opencv::core::no_array().unwrap(),
+            &mut opencv::core::no_array().unwrap(),
         )
         .unwrap();
 
-        PnPResult {
-            intrinsic_matrix,
-            distortion_coeffs,
-            rvec_mat,
-            tvec_mat,
-        }
+        rvec_mats.iter().zip(tvec_mats).map(|(rvec_mat, tvec_mat)| {
+            PnPResult {
+                intrinsic_matrix: Mat::copy(&intrinsic_matrix).unwrap(),
+                distortion_coeffs: Mat::copy(&distortion_coeffs).unwrap(),
+                rvec_mat,
+                tvec_mat,
+            }
+        }).collect()
     }
 
-    pub fn find_target(&self, id: u8, pnp_result: &PnPResult, config: &CameraConfig) -> Target {
-        let PnPResult {
-            rvec_mat, tvec_mat, ..
-        } = pnp_result;
+    pub fn find_target(&self, id: u8, pnp_results: &Vec<PnPResult>, config: &CameraConfig) -> Target {
+        let (rmat, tvec, camera_pose) = pnp_results.iter().find_map(move |pnp_result| {
+            let PnPResult {
+                rvec_mat, tvec_mat, ..
+            } = pnp_result;
 
-        let mut rmat_mat = Mat::default().unwrap();
-        let mut jacobian_mat = Mat::default().unwrap();
-        rodrigues(&rvec_mat, &mut rmat_mat, &mut jacobian_mat).unwrap();
+            let mut rmat_mat = Mat::default().unwrap();
+            let mut jacobian_mat = Mat::default().unwrap();
+            rodrigues(&rvec_mat, &mut rmat_mat, &mut jacobian_mat).unwrap();
 
-        let tvec = tvec_mat.as_array_view::<f64>().into_shape((3, 1)).unwrap();
-        let rmat = rmat_mat.as_array_view::<f64>().into_shape((3, 3)).unwrap();
-        let rmat_t = rmat.t();
+            let tvec = tvec_mat.as_array_view::<f64>().into_shape((3, 1)).unwrap();
+            let rmat = rmat_mat.as_array_view::<f64>().into_shape((3, 3)).unwrap();
+            let rmat_t = rmat.t();
 
-        let camera_pose = rmat_t.dot(&tvec);
+            let camera_pose = rmat_t.dot(&tvec);
+
+            if tvec[[2, 0]].is_sign_negative() {
+                return None;
+            }
+
+            Some((rmat.into_owned(), tvec.into_owned(), camera_pose))
+        }).expect("Failed to find valid PnP solution");
 
         let x = camera_pose[[0, 0]];
         let y = camera_pose[[1, 0]];
@@ -153,22 +199,8 @@ impl WallTapeContourAnalyzer {
 
 impl ContourAnalyzer for WallTapeContourAnalyzer {
     fn analyze(&self, contour_group: &ContourGroup) -> Target {
-        let obj_points = match contour_group.id {
-            0 => &self.high_port_object_points,
-            1 => &self.loading_port_object_points,
-            _ => panic!("Unknown contour group type"),
-        };
-
-        let img_points = VectorOfPoint2f::from_iter(
-            contour_group
-                .contours
-                .iter()
-                .map(|contour| contour.points.iter())
-                .flatten()
-                .map(|point| Point2f::new(point.0 as f32, point.1 as f32)),
-        );
-
-        let pnp_result = self.solve_pnp(&img_points, &obj_points, contour_group.camera);
+        let pnp_params = self.make_pnp_params(contour_group);
+        let pnp_result = self.solve_pnp(pnp_params);
 
         self.find_target(contour_group.id, &pnp_result, contour_group.camera)
     }
