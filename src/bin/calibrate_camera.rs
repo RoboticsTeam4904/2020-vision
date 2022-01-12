0e1b8aa792fa5cdb78c8fd312e19d95e1d9c84ce
aemino <aeminodev@gmail.com>
2022-01-12 15:24:15 -0800

    use `anyhow` in scripts, start converting to `clap`

--- src/bin/calibrate_camera.rs
+++ src/bin/calibrate_camera.rs
@@ -1,18 +1,18 @@
 use std::{env, fs, io::prelude::*};
 
+use anyhow::{bail, Context, Result};
 use opencv::{
-    calib3d::{calibrate_camera, find_chessboard_corners_sb},
+    calib3d,
     core::{Mat, Point3f, Size, TermCriteria, TermCriteria_Type},
-    imgcodecs::{imread, IMREAD_COLOR},
+    imgcodecs::{self, IMREAD_COLOR},
     prelude::*,
     types::{VectorOfPoint2f, VectorOfPoint3f, VectorOfVectorOfPoint2f, VectorOfVectorOfPoint3f},
 };
+use serde::{Deserialize, Serialize};
+use serde_json;
 
 use stdvis_core::types::CameraConfig;
 use stdvis_opencv::convert::AsArrayView;
-
-use serde::{Deserialize, Serialize};
-use serde_json;
 
 #[derive(Default, Serialize, Deserialize)]
 struct Config {
@@ -22,7 +22,7 @@ struct Config {
     camera: CameraConfig,
 }
 
-fn calculate_obj_points(square_size: f32, width: u8, height: u8) -> Vec<Point3f> {
+fn compute_checkerboard_obj_points(square_size: f32, width: u8, height: u8) -> Vec<Point3f> {
     let mut obj_points = Vec::with_capacity((width * height) as usize);
 
     for row in 0..height {
@@ -37,7 +37,8 @@ fn calculate_obj_points(square_size: f32, width: u8, height: u8) -> Vec<Point3f>
 
     obj_points
 }
-fn main() {
+
+fn main() -> Result<()> {
     let mut args = env::args().skip(1);
 
     let config_path = args.next().expect("Expected config path as first arg");
@@ -53,16 +54,18 @@ fn main() {
         .write(true)
         .create(true)
         .open(&config_path)
-        .unwrap();
+        .context("opening config file")?;
 
     let mut config_str = String::new();
-    config_file.read_to_string(&mut config_str).unwrap();
+    config_file
+        .read_to_string(&mut config_str)
+        .context("reading config file")?;
 
     if config_str.is_empty() {
-        serde_json::to_writer_pretty(config_file, &Config::default()).unwrap();
+        serde_json::to_writer_pretty(config_file, &Config::default())
+            .context("writing template config file")?;
 
-        println!("Please provide camera configuration and target parameters.");
-        return;
+        bail!("Please provide camera configuration and target parameters.");
     }
 
     let mut config: Config = match serde_json::from_str(&config_str) {
@@ -73,7 +76,7 @@ fn main() {
     };
 
     let num_images = image_paths.len();
-    let template_obj_points = calculate_obj_points(
+    let template_obj_points = compute_checkerboard_obj_points(
         config.square_size_mm / 1000.,
         config.board_rows,
         config.board_cols,
@@ -86,31 +89,31 @@ fn main() {
     let image_size = Size::new(resolution.0 as i32, resolution.1 as i32);
 
     for path in image_paths {
-        let image = imread(&path, IMREAD_COLOR).unwrap();
+        let image = imgcodecs::imread(&path, IMREAD_COLOR).context("reading image from disk")?;
 
         if image.size().unwrap() != image_size {
-            panic!("Expected all images to be the same size");
+            bail!("Expected all images to be the same size");
         }
 
         let mut corners = VectorOfPoint2f::new();
-        find_chessboard_corners_sb(
+        calib3d::find_chessboard_corners_sb(
             &image,
             Size::new(config.board_rows as i32, config.board_cols as i32),
             &mut corners,
             0,
         )
-        .expect("Can't find chessboard corners");
+        .context("failed to find checkerboard corners")?;
 
         obj_points.push(VectorOfPoint3f::from_iter(template_obj_points.clone()));
         img_points.push(corners);
     }
 
-    let mut camera_matrix = Mat::default().unwrap();
-    let mut dist_coeffs = Mat::default().unwrap();
-    let mut rvecs = Mat::default().unwrap();
-    let mut tvecs = Mat::default().unwrap();
+    let mut camera_matrix = Mat::default();
+    let mut dist_coeffs = Mat::default();
+    let mut rvecs = Mat::default();
+    let mut tvecs = Mat::default();
 
-    calibrate_camera(
+    calib3d::calibrate_camera(
         &VectorOfVectorOfPoint3f::from_iter(obj_points),
         &VectorOfVectorOfPoint2f::from_iter(img_points),
         image_size,
@@ -119,6 +122,7 @@ fn main() {
         &mut rvecs,
         &mut tvecs,
         0,
+        // Listed as the default TermCriteria for this method in the OpenCV docs.
         TermCriteria::new(
             TermCriteria_Type::COUNT as i32 + TermCriteria_Type::EPS as i32,
             30,
@@ -126,25 +130,27 @@ fn main() {
         )
         .unwrap(),
     )
-    .unwrap();
+    .context("calibrating camera")?;
 
     config.camera.intrinsic_matrix = camera_matrix
         .as_array_view::<f64>()
         .into_shape((3, 3))
-        .unwrap()
+        .context("converting intrinsic_matrix Mat")?
         .to_owned();
 
     config.camera.distortion_coeffs = dist_coeffs
         .as_array_view::<f64>()
         .into_shape(5)
-        .unwrap()
+        .context("converting distortion_coeffs Mat")?
         .to_owned();
 
     let config_file = fs::OpenOptions::new()
         .truncate(true)
         .write(true)
         .open(&config_path)
-        .unwrap();
+        .context("opening config file")?;
 
-    serde_json::to_writer_pretty(config_file, &config).unwrap();
+    serde_json::to_writer_pretty(config_file, &config).context("writing updated config file")?;
+
+    Ok(())
 }
--- src/bin/sample_images.rs
+++ src/bin/sample_images.rs
@@ -1,11 +1,15 @@
+use std::{fs, io::prelude::*, path::PathBuf};
+
+use anyhow::{bail, Context, Result};
+use clap::Parser;
+use opencv::imgcodecs;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use stdvis_core::{
     traits::Camera,
     types::{CameraConfig, VisionTarget},
 };
 use stdvis_opencv::{camera::OpenCVCamera, convert::AsMatView};
-
-use serde::{Deserialize, Serialize};
-use serde_json;
 
 #[derive(Default, Serialize, Deserialize)]
 struct Params {
@@ -27,19 +31,35 @@ struct ImageMetadata {
     exposure: f64,
 }
 
-fn main() {
-    const IN_FILE: &str = "params.json";
+// Captures a set of sample images.
+#[derive(Debug, Parser)]
+#[clap(about)]
+struct Args {
+    /// Specifies the amount of time, in ms, to wait between captures.
+    #[clap(short, long, name = "delay")]
+    delay_ms: Option<u64>,
+
+    /// The path to read input parameters for.
+    /// If the target file does not exist, a template will be created upon the first run.
+    #[clap(parse(from_os_str))]
+    params_file: PathBuf,
+
+    /// The directory to output images and corresponding metadata to.
+    #[clap(parse(from_os_str))]
+    output_dir: PathBuf,
+}
+
+fn main() -> Result<()> {
     const METADATA_FILE: &str = "metadata.json";
 
-    let mut args = env::args().skip(1);
-    let out_path = args.next().expect("Expected an output directory path");
+    let args = Args::try_parse()?;
 
     let mut params_file = fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .open(format!("{}/{}", out_path, IN_FILE))
-        .unwrap();
+        .open(args.params_file)
+        .context("failed open params file")?;
 
     let mut params_str = String::new();
     params_file.read_to_string(&mut params_str).unwrap();
@@ -47,28 +67,30 @@ fn main() {
     if params_str.is_empty() {
         serde_json::to_writer_pretty(params_file, &Params::default()).unwrap();
 
-        println!("Please configure input parameters.");
-        return;
+        bail!(
+            "Please configure input parameters.
+            A template file has been created at the specified path."
+        );
     }
 
     let params: Params = match serde_json::from_str(&params_str) {
         Ok(params) => params,
         Err(_) => {
-            panic!("Failed to parse input parameters. The schema used may be out of date.");
+            bail!(
+                "Failed to parse input parameters.
+                The config file may be malformed or out of date.
+                You may want to let the script generate a new template."
+            );
         }
     };
 
     let mut camera = OpenCVCamera::new(params.camera).unwrap();
     let camera_config = camera.config().clone();
 
-    use std::{thread, time};
+    use std::{thread, time::Duration};
 
     // Allow the camera to "warm up."
-    thread::sleep(time::Duration::from_millis(1000));
-
-    use std::env;
-    use std::fs;
-    use std::io::prelude::*;
+    thread::sleep(Duration::from_millis(1000));
 
     let mut metadata_file = fs::OpenOptions::new()
         .read(true)
@@ -83,22 +105,26 @@ fn main() {
     let mut metadata =
         serde_json::from_str(&metadata_str).unwrap_or(Metadata { images: Vec::new() });
 
+    let delay = args.delay_ms.map(|ms| Duration::from_millis(ms));
+    let output_dir = args.output_dir;
+
     for idx in 0..10 {
         camera.set_exposure(-(idx as f64) * 0.1).unwrap();
 
         let frame = camera.grab_frame().unwrap();
         let image_mat = frame.as_mat_view();
 
-        use opencv::{imgcodecs, prelude::*};
-
         let index = metadata.images.len();
 
         imgcodecs::imwrite(
-            format!("{}/{}_{}.png", out_path, params.label, index).as_str(),
+            output_dir
+                .join(format!("{}_{}", params.label, index))
+                .to_str()
+                .unwrap(),
             &*image_mat,
             &opencv::types::VectorOfi32::with_capacity(0),
         )
-        .unwrap();
+        .context("writing image to disk")?;
 
         metadata.images.push(ImageMetadata {
             index,
@@ -106,13 +132,18 @@ fn main() {
             config: camera_config.clone(),
             exposure: camera.exposure().unwrap().clone(),
         });
+
+        if let Some(delay) = delay {
+            thread::sleep(delay);
+        }
     }
 
     let metadata_file = fs::OpenOptions::new()
         .truncate(true)
         .write(true)
-        .open("sample-images/metadata.json")
-        .unwrap();
+        .open("sample-images/metadata.json")?;
 
-    serde_json::to_writer_pretty(metadata_file, &metadata).unwrap();
+    serde_json::to_writer_pretty(metadata_file, &metadata)?;
+
+    Ok(())
 }
