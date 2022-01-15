ce37590c8afed794ee23e405cba9ea570a357175
IanLum <ianlum314@gmail.com>
2022-01-14 18:07:20 -0800

    create branch

---
+++ src/bin/run_pipeline.rs
@@ -0,0 +1,15 @@
+// mod control_panel;
+// mod extraction;
+// mod analysis;
+
+use vision_2020::pipeline::VisionPipeline;
+use stdvis_core::types::{CameraConfig, Pose};
+
+fn main() {
+    let config = CameraConfig 
+    let pipeline = VisionPipeline::new(
+        camera: CameraConfig,
+        extractor,
+        analyzer
+    )
+}
