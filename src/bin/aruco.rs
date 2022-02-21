ee7d0f8f7b93c7b13fb72b86afc253948d6f0eee
IanLum <ianlum314@gmail.com>
2022-02-21 10:31:18 -0800

    refactor `aruco.rs`

--- src/bin/aruco.rs
+++ src/bin/aruco.rs
@@ -11,11 +11,172 @@ use opencv::{
 };
 use serde_json;
 use std::{f64::consts::PI, fs::File, ops::DerefMut};
-use stdvis_core::{traits::Camera, types::VisionTarget};
+use stdvis_core::{
+    traits::Camera,
+    types::{Image, VisionTarget},
+};
 use stdvis_opencv::{
-    camera::OpenCVCamera,
+    camera::{MatImageData, OpenCVCamera},
     convert::{AsArrayView, AsMatView},
 };
+pub struct ArucoPoseResult {
+    pub intrinsic_matrix: Mat,
+    pub distortion_coeffs: Mat,
+    pub rvecs: Vector<Vec3d>,
+    pub tvecs: Vector<Vec3d>,
+    pub ids: Vector<i32>,
+    pub corners: Vector<Vector<Point2f>>,
+}
+
+fn find_pose(image: &Image<MatImageData>) -> Result<ArucoPoseResult> {
+    let mat_image = image.as_mat_view();
+    let dictionary =
+        aruco::get_predefined_dictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50)?;
+    let mut corners = Vector::<Vector<Point2f>>::new();
+    let mut ids = Vector::<i32>::new();
+    let params = DetectorParameters::create()?;
+    let mut rejected_img_points = no_array();
+
+    let i = image.camera.intrinsic_matrix.view();
+    let d = image.camera.distortion_coeffs.view();
+
+    let intrinsic_matrix = Mat::from_slice_2d(&[
+        &[i[[0, 0]], i[[0, 1]], i[[0, 2]]],
+        &[i[[1, 0]], i[[1, 1]], i[[1, 2]]],
+        &[i[[2, 0]], i[[2, 1]], i[[2, 2]]],
+    ])?;
+
+    let distortion_coeffs = Mat::from_slice(d.as_slice().unwrap())?;
+
+    detect_markers(
+        &*mat_image,
+        &dictionary,
+        &mut corners,
+        &mut ids,
+        &params,
+        &mut rejected_img_points,
+        &intrinsic_matrix,
+        &distortion_coeffs,
+    )?;
+
+    let mut rvecs = Vector::<Vec3d>::new();
+    let mut tvecs = Vector::<Vec3d>::new();
+
+    estimate_pose_single_markers(
+        &corners,
+        0.036,
+        &intrinsic_matrix,
+        &distortion_coeffs,
+        &mut rvecs,
+        &mut tvecs,
+        &mut no_array(),
+    )?;
+
+    Ok(ArucoPoseResult {
+        intrinsic_matrix,
+        distortion_coeffs,
+        rvecs,
+        tvecs,
+        corners,
+        ids,
+    })
+}
+
+fn find_targets(aruco_result: &ArucoPoseResult) -> Result<Vec<VisionTarget>> {
+    let mut targets = Vec::new();
+
+    for idx in 0..aruco_result.ids.len() {
+        let rvec_vec = aruco_result.rvecs.get(idx)?;
+        let tvec_vec = aruco_result.tvecs.get(idx)?;
+
+        let mut rmat_mat = Mat::default();
+        let mut jacobian_mat = Mat::default();
+        rodrigues(&rvec_vec, &mut rmat_mat, &mut jacobian_mat)?;
+
+        let tvec_mat = Mat::from_exact_iter(tvec_vec.into_iter())?;
+        // let tvec = tvec_mat.as_array_view::<f64>().into_shape((3, 1))?;
+
+        let mut mat_array = VectorOfMat::new();
+        mat_array.push(rmat_mat);
+        mat_array.push(tvec_mat);
+
+        let mut proj_mat = Mat::default();
+        hconcat(&mat_array, &mut proj_mat)?;
+
+        let mut camera_matrix = Mat::default();
+        let mut rot_matrix = Mat::default();
+        let mut trans_vect = Mat::default();
+        let mut rot_matrix_x = Mat::default();
+        let mut rot_matrix_y = Mat::default();
+        let mut rot_matrix_z = Mat::default();
+        let mut euler_angles_mat = Mat::default();
+        decompose_projection_matrix(
+            &proj_mat,
+            &mut camera_matrix,
+            &mut rot_matrix,
+            &mut trans_vect,
+            &mut rot_matrix_x,
+            &mut rot_matrix_y,
+            &mut rot_matrix_z,
+            &mut euler_angles_mat,
+        )?;
+
+        let x = tvec_vec.get(0).unwrap();
+        let y = tvec_vec.get(1).unwrap();
+        let z = tvec_vec.get(2).unwrap();
+
+        let theta = x.atan2(*z) * 180. / PI;
+
+        let euler_angles = euler_angles_mat.as_array_view::<f64>().into_shape((3, 1))?;
+        let roll = euler_angles[[2, 0]];
+        let pitch = euler_angles[[0, 0]];
+        let yaw = euler_angles[[1, 0]];
+
+        let target = VisionTarget {
+            id: 0,
+            theta: theta,
+            beta: yaw,
+            dist: *z,
+            height: *y,
+            confidence: 0.,
+        };
+
+        targets.push(target);
+    }
+    Ok(targets)
+}
+
+fn write_poses(image: &Image<MatImageData>, aruco_result: &ArucoPoseResult) -> Result<()> {
+    let mut mat_image = image.as_mat_view();
+    let mut out_img = mat_image.deref_mut();
+
+    draw_detected_markers(
+        &mut out_img,
+        &aruco_result.corners,
+        &aruco_result.ids,
+        Scalar::new(255.0, 0.0, 0.0, 0.0),
+    )?;
+
+    for idx in 0..aruco_result.ids.len() {
+        opencv::calib3d::draw_frame_axes(
+            out_img,
+            &aruco_result.intrinsic_matrix,
+            &aruco_result.distortion_coeffs,
+            &aruco_result.rvecs.get(idx)?,
+            &aruco_result.tvecs.get(idx)?,
+            0.05,
+            3,
+        )?;
+    }
+
+    opencv::imgcodecs::imwrite(
+        "aruco.png",
+        out_img,
+        &opencv::types::VectorOfi32::with_capacity(0),
+    )?;
+
+    Ok(())
+}
 
 fn main() -> Result<()> {
     let config_file = File::open("config.json")?;
@@ -27,134 +188,13 @@ fn main() -> Result<()> {
             .grab_frame()
             .context("Failed to read frame from camera")?;
 
-        let mut mat_image = image.as_mat_view();
-        let dictionary =
-            aruco::get_predefined_dictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50)?;
-        let mut corners = Vector::<Vector<Point2f>>::new();
-        let mut ids = Vector::<i32>::new();
-        let params = DetectorParameters::create()?;
-        let mut rejected_img_points = no_array();
+        let aruco_result = find_pose(&image)?;
+        let targets = find_targets(&aruco_result)?;
+        write_poses(&image, &aruco_result)?;
 
-        let i = image.camera.intrinsic_matrix.view();
-        let d = image.camera.distortion_coeffs.view();
-
-        let intrinsic_matrix = Mat::from_slice_2d(&[
-            &[i[[0, 0]], i[[0, 1]], i[[0, 2]]],
-            &[i[[1, 0]], i[[1, 1]], i[[1, 2]]],
-            &[i[[2, 0]], i[[2, 1]], i[[2, 2]]],
-        ])?;
-
-        let distortion_coeffs = Mat::from_slice(d.as_slice().unwrap())?;
-
-        detect_markers(
-            &*mat_image,
-            &dictionary,
-            &mut corners,
-            &mut ids,
-            &params,
-            &mut rejected_img_points,
-            &intrinsic_matrix,
-            &distortion_coeffs,
-        )?;
-
-        let mut out_img = mat_image.deref_mut();
-
-        let mut rvecs = Vector::<Vec3d>::new();
-        let mut tvecs = Vector::<Vec3d>::new();
-
-        draw_detected_markers(
-            &mut out_img,
-            &corners,
-            &ids,
-            Scalar::new(255.0, 0.0, 0.0, 0.0),
-        )?;
-
-        estimate_pose_single_markers(
-            &corners,
-            0.036,
-            &intrinsic_matrix,
-            &distortion_coeffs,
-            &mut rvecs,
-            &mut tvecs,
-            &mut no_array(),
-        )?;
-
-        for idx in 0..ids.len() {
-            let rvec_vec = rvecs.get(idx)?;
-            let tvec_vec = tvecs.get(idx)?;
-
-            opencv::calib3d::draw_frame_axes(
-                out_img,
-                &intrinsic_matrix,
-                &distortion_coeffs,
-                &rvecs.get(idx)?,
-                &tvecs.get(idx)?,
-                0.05,
-                3,
-            )?;
-
-            let mut rmat_mat = Mat::default();
-            let mut jacobian_mat = Mat::default();
-            rodrigues(&rvec_vec, &mut rmat_mat, &mut jacobian_mat)?;
-
-            let tvec_mat = Mat::from_exact_iter(tvec_vec.into_iter())?;
-            // let tvec = tvec_mat.as_array_view::<f64>().into_shape((3, 1))?;
-
-            let mut mat_array = VectorOfMat::new();
-            mat_array.push(rmat_mat);
-            mat_array.push(tvec_mat);
-
-            let mut proj_mat = Mat::default();
-            hconcat(&mat_array, &mut proj_mat)?;
-
-            let mut camera_matrix = Mat::default();
-            let mut rot_matrix = Mat::default();
-            let mut trans_vect = Mat::default();
-            let mut rot_matrix_x = Mat::default();
-            let mut rot_matrix_y = Mat::default();
-            let mut rot_matrix_z = Mat::default();
-            let mut euler_angles_mat = Mat::default();
-            decompose_projection_matrix(
-                &proj_mat,
-                &mut camera_matrix,
-                &mut rot_matrix,
-                &mut trans_vect,
-                &mut rot_matrix_x,
-                &mut rot_matrix_y,
-                &mut rot_matrix_z,
-                &mut euler_angles_mat,
-            )?;
-
-            let x = tvec_vec.get(0).unwrap();
-            let y = tvec_vec.get(1).unwrap();
-            let z = tvec_vec.get(2).unwrap();
-
-            let theta = x.atan2(*z) * 180. / PI;
-
-            let euler_angles = euler_angles_mat.as_array_view::<f64>().into_shape((3, 1))?;
-            let roll = euler_angles[[2, 0]];
-            let pitch = euler_angles[[0, 0]];
-            let yaw = euler_angles[[1, 0]];
-
-            let target = VisionTarget {
-                id: 0,
-                theta: theta,
-                beta: yaw,
-                dist: *z,
-                height: *y,
-                confidence: 0.,
-            };
-
-            dbg!(target);
-        }
+        dbg!(targets);
 
         println!("-------------------------------");
-
-        opencv::imgcodecs::imwrite(
-            "aruco.png",
-            out_img,
-            &opencv::types::VectorOfi32::with_capacity(0),
-        )?;
 
         std::thread::sleep(std::time::Duration::from_millis(200));
     }
