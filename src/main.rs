2947e730a1993de8a0ab34cea6d1851e0b753b34
countableclouds <aksharramkumar@gmail.com>
2022-03-21 15:39:25 -0700

    change time units to microseconds and remove image udp

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() -> Result<()> {
     let config_file = File::open("config.json")?;
     let config = serde_json::from_reader(config_file)?;
 
-    let mut camera = OpenCVCamera::new(config)?;
+    let camera = OpenCVCamera::new(config)?;
     let extractor = extraction::RFTapeContourExtractor::new();
     let analyzer = analysis::WallTapeContourAnalyzer::new();
 
@@ -20,22 +20,22 @@ fn main() -> Result<()> {
 
     // TODO: Consider moving destination hostname to environment variable.
     // This line will block until the hostname appears on the network.
-    let localization_sender = UdpSender::new(4904, "nano2-4904-frc.local:4826".to_string());
-    let driverstation_sender = UdpSender::new(4905, "DESKTOP-E0B7IEK".to_string());
+    println!("Initializing UDP sensor.");
+    let sender = UdpSender::new(7698, "nano2-4904-frc.local:2746".to_string());
+    println!("UDP sensor initialized.");
 
     loop {
         let target = pipeline.run()?;
         dbg!(&target);
-
-        localization_sender.send((
-            SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs_f64(),
-            target,
-        ));
-
-        std::thread::sleep(std::time::Duration::from_millis(200));
+        sender
+            .send((
+                target,
+                SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_micros() as u64,
+            ))
+            .unwrap();
     }
 
     Ok(())
